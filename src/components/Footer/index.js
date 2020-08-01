549670e971c2ef03195fc3b142ed6b1697388502
Gabriel Piccoli <gabriel.pdmarcos@gmail.com>
2020-07-31 21:38:47 -0300

    Alteração do Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Facebook from '../../assets/img/logo-facebook.png';
+import Linkedin from '../../assets/img/logo-linkedin.png';
+import Github from '../../assets/img/logo-github.png';
 import { FooterBase } from './styles';
 
 function Footer() {
@@ -11,6 +14,17 @@ function Footer() {
           Imersão React da Alura
         </a>
       </p>
+      <div className="icones">
+        <a href="https://www.facebook.com/gabriel.piccoli.1">
+          <img className="icones-sociais" src={Facebook}></img>
+        </a>
+        <a href="https://github.com/GabrielPiccoli">
+          <img className="icones-sociais" src={Github}></img>
+        </a>
+        <a href="https://www.linkedin.com/in/gabriel-piccoli-144756103/">
+          <img className="icones-sociais" src={Linkedin}></img>
+        </a>
+      </div>
     </FooterBase>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
 
-
 export const FooterBase = styled.footer`
+  display: flex;
+  justify-content: space-between;
   background: var(--black);
+  opacity: 0.9;
   border-top: 2px solid var(--primary);
-  padding-left: 16px;
-  padding-right: 16px;
+  padding-left: 30px;
+  padding-right: 30px;
   padding-top: 10px;
   padding-bottom: 10px;
   color: var(--white);
   text-align: center;
   @media (max-width: 800px) {
     margin-bottom: 50px;
+  }
+
+  .icones-sociais {
+    width: 40px;
+    margin-right: 10px;
   }
 `;
